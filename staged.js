745f913b84f1e68bab56f7c1e8add808a8606260
Patsagorn <patsagorn_y@outlook.com>
2021-10-15 02:06:57 +0700

    update

---
+++ staged.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Patsagorn Y.
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+let root = "http://127.0.0.1:8080"
+$( document ).ready(function() {
+  mw.loader.load(`${root}/src/edit-requester.js`)
+});
